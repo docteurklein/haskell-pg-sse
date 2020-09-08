f3b8ea3f909f341d6c2d6bb215f70a4aa9f6ac48
Florian Klein <florian.klein@free.fr>
2020-09-08 17:19:12 +0200

    accept many params and return many rows

--- Main.hs
+++ Main.hs
@@ -6,10 +6,9 @@ import Data.ByteString.Char8 (pack)
 import Data.ByteString (ByteString(..))
 import Data.Text (Text(..), unpack)
 import Data.Text.Encoding (encodeUtf8)
-import Control.Concurrent.Async (async, withAsync)
--- import Control.Concurrent (forkIO)
+import Control.Concurrent.Async (async)
 import Control.Concurrent.Chan
-import Control.Monad (forever, void)
+import Control.Monad (forever, void, forM_)
 import Network.Wai (Application, Middleware)
 import Network.Wai.EventSource (ServerEvent(..), eventSourceAppChan)
 import Network.Wai.Handler.Warp (run)
@@ -17,17 +16,19 @@ import Network.Wai.Middleware.AddHeaders (addHeaders)
 import Network.Wai.Middleware.Gzip (gzip, def)
 import Hasql.Connection (acquire, Connection)
 import Hasql.Statement (Statement(..))
-import qualified Hasql.Encoders as Encoders
-import qualified Hasql.Decoders as Decoders
+import qualified Hasql.Encoders as E
+import qualified Hasql.Decoders as D
 import qualified Hasql.Session as Session
 import Hasql.Notifications (listen, unlisten, toPgIdentifier, waitForNotifications)
 import GHC.Generics
 import Data.Aeson
+import System.IO (hPutStrLn, stderr)
 
 
 data Query = Query {
     sql :: Text
   , params :: [Text]
+  , role :: Maybe Text
 } deriving (Generic, Show)
 
 instance FromJSON Query
@@ -38,18 +39,21 @@ eventChan connListen conn chan = waitForNotifications (handler conn) connListen
         handler conn channel payload = do
             let query = decode (fromStrict payload) :: Maybe Query
             case query of
+                Nothing -> do
+                    hPutStrLn stderr "invalid payload"
+                    print payload
                 Just query' -> do
-                    result <- Session.run (Session.statement (head $ params query') (select $ sql query')) conn
+                    result <- Session.run (Session.statement (params query') (select $ sql query')) conn
                     case result of
-                        Left e -> print e
-                        Right content -> do
-                            print $ content
-                            writeChan chan (ServerEvent Nothing Nothing [string8 $ unpack content])
-                Nothing -> print payload
+                        Left e -> do
+                            hPutStrLn stderr "invalid result"
+                            print e
+                        Right rows -> do
+                            forM_ rows $ \x -> writeChan chan (ServerEvent Nothing Nothing [string8 $ unpack x])
 
         select sql = Statement (encodeUtf8 sql) encoder decoder True where
-          encoder = Encoders.param (Encoders.nonNullable Encoders.text)
-          decoder = Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.text))
+          encoder = E.param (E.nonNullable (E.foldableArray (E.nonNullable E.text)))
+          decoder = D.rowVector (D.column (D.nonNullable D.text))
 
 main :: IO ()
 main = do
@@ -64,8 +68,4 @@ main = do
       headers = addHeaders [
           ("X-Accel-Buffering", "no")
         , ("Cache-Control", "no-cache")
-        , ("Access-Control-Allow-Origin", "*")
-        , ("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE")
-        , ("Access-Control-Allow-Headers", "Authorization, Accept")
-        , ("Access-Control-Expose-Headers", "Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma, Link")
         ]
