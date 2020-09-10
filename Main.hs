3f95f2619d1fa4d7ce0dad99cf94740c84e252c2
Florian Klein <florian.klein@free.fr>
2020-09-10 12:26:07 +0200

    multi topic

--- Main.hs
+++ Main.hs
@@ -1,23 +1,24 @@
 module Main where
 
 import Control.Concurrent.Async (async)
-import Control.Concurrent.Chan
-import Control.Monad (forever, void, forM_)
+import Control.Concurrent.Chan (dupChan, newChan, readChan, writeChan, Chan)
+import Control.Monad (forever, void, forM_, when)
+import Control.Monad.IO.Class (liftIO)
 import Data.Aeson (FromJSON, decode)
 import Data.ByteString (ByteString(..))
 import Data.ByteString.Builder (string8)
 import Data.ByteString.Char8 (pack)
 import Data.ByteString.Lazy.Char8 (fromStrict)
 import Data.Text (Text(..), unpack)
-import Data.Text.Encoding (encodeUtf8)
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import GHC.Generics (Generic)
 import Hasql.Connection (acquire, Connection)
 import Hasql.Decoders (rowVector, column)
 import Hasql.Encoders (param, foldableArray, nonNullable, text)
 import Hasql.Notifications (listen, unlisten, toPgIdentifier, waitForNotifications)
 import Hasql.Statement (Statement(..))
-import Network.Wai (Application, Middleware)
-import Network.Wai.EventSource (ServerEvent(..), eventSourceAppChan)
+import Network.Wai (Application, Middleware, pathInfo)
+import Network.Wai.EventSource (ServerEvent(..), eventSourceAppIO)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.AddHeaders (addHeaders)
 import Network.Wai.Middleware.Gzip (gzip, def)
@@ -29,14 +30,15 @@ import qualified Hasql.Session as Session
 data Query = Query {
     sql :: Text
   , params :: [Text]
-  , role :: Maybe Text
+  --, token :: Token
 } deriving stock    (Generic, Show)
   deriving anyclass FromJSON
 
-eventChan :: Connection -> Connection -> Chan ServerEvent -> IO ()
-eventChan connListen conn chan = waitForNotifications (handler conn) connListen
+eventChan :: Text -> Connection -> Connection -> Chan ServerEvent -> IO ()
+eventChan topic connListen conn chan = waitForNotifications (handler conn) connListen
     where
         handler conn channel payload = do
+            when (channel /= encodeUtf8 topic) $ return ()
             let query = decode (fromStrict payload) :: Maybe Query
             case query of
                 Nothing -> do
@@ -57,15 +59,22 @@ eventChan connListen conn chan = waitForNotifications (handler conn) connListen
 
 main :: IO ()
 main = do
-    chan <- newChan
     Right conn <- acquire ""
-    Right connListen <- acquire ""
-    listen connListen $ toPgIdentifier "*"
-    async $ (eventChan connListen conn) chan
-    run 8080 (gzip def $ headers $ eventSourceAppChan chan)
+    hPutStrLn stderr "listening http on 0.0.0.0:8080"
+    run 8080 $ gzip def $ headers $ eventSourceAppChan conn
     where
-      headers :: Middleware
-      headers = addHeaders [
-          ("X-Accel-Buffering", "no")
-        , ("Cache-Control", "no-cache")
-        ]
+        eventSourceAppChan conn req sendResponse = do
+            chan <- newChan
+            forM_ (pathInfo req) $ \topic -> do
+                Right connListen <- acquire ""
+                listen connListen $ toPgIdentifier topic
+                print $ "listening topic: "
+                print topic
+                void $ async $ eventChan topic connListen conn chan
+            eventSourceAppIO (readChan chan) req sendResponse
+
+        headers :: Middleware
+        headers = addHeaders [
+            ("X-Accel-Buffering", "no")
+          , ("Cache-Control", "no-cache")
+          ]
