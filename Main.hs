648c079288e7354acc2f0a570684e03bdd011d9f
Florian Klein <florian.klein@free.fr>
2021-03-15 16:58:17 +0100

    wip

--- Main.hs
+++ Main.hs
@@ -1,24 +1,21 @@
 module Main where
 
 import Control.Concurrent.Async (async)
-import Control.Concurrent.Chan (dupChan, newChan, readChan, writeChan, Chan)
-import Control.Monad (forever, void, forM_, when)
-import Control.Monad.IO.Class (liftIO)
+import Control.Concurrent.Chan (newChan, readChan, Chan)
+import Control.Monad (void, forM_, when)
 import Data.Aeson (FromJSON, decode)
-import Data.ByteString (ByteString(..))
 import Data.ByteString.Builder (string8)
-import Data.ByteString.Char8 (pack)
 import Data.ByteString.Lazy.Char8 (fromStrict)
 import Data.Pool (createPool, withResource)
 import Data.Text (Text(..), unpack)
-import Data.Text.Encoding (encodeUtf8, decodeUtf8)
+import Data.Text.Encoding (encodeUtf8)
 import GHC.Generics (Generic)
 import Hasql.Connection (acquire, release, Connection)
 import Hasql.Decoders (rowVector, column)
 import Hasql.Encoders (param, foldableArray, nonNullable, text)
-import Hasql.Notifications (listen, unlisten, toPgIdentifier, waitForNotifications)
+import Hasql.Notifications (listen, toPgIdentifier, waitForNotifications)
 import Hasql.Statement (Statement(..))
-import Network.Wai (Application, Middleware, pathInfo)
+import Network.Wai (Middleware, pathInfo)
 import Network.Wai.EventSource (ServerEvent(..), eventSourceAppIO)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.AddHeaders (addHeaders)
@@ -35,20 +32,20 @@ data Query = Query {
 } deriving stock    (Generic, Show)
   deriving anyclass FromJSON
 
-listenPostgres :: Text -> Connection -> Chan Query -> IO ()
-listenPostgres topic connListen chan = waitForNotifications handler connListen
+listenPostgres :: Text -> Connection -> Connection -> Chan ServerEvent -> IO ()
+listenPostgres topic connListen conn _ = waitForNotifications (handler conn) connListen
     where
-        handler channel payload = do
+        handler conn channel payload = do
             when (channel /= encodeUtf8 topic) $ return ()
             let query = decode (fromStrict payload) :: Maybe Query
-            maybe id (writeChan chan)
-            -- case query of
-            --     Nothing -> do
-            --         hPutStrLn stderr "invalid payload"
-            --         print payload
-            --     Just query' -> do
-            --         result <- Session.run (Session.statement (params query') (select $ sql query')) conn
-            --         either print (\rows -> forM_ rows $ writeChan chan) result
+            -- maybe id (writeChan chan)
+            case query of
+                Nothing -> do
+                    hPutStrLn stderr "invalid payload"
+                    print payload
+                Just query' -> do
+                    result <- Session.run (Session.statement (params query') (select $ sql query')) conn
+                    either print (\rows -> forM_ rows $ \payload -> return (ServerEvent Nothing Nothing [string8 $ unpack payload])) result
 
         select sql = Statement (encodeUtf8 sql) encoder decoder True where
           encoder = param $ nonNullable $ foldableArray $ nonNullable text
@@ -72,12 +69,12 @@ main = do
                 listen connListen $ toPgIdentifier topic
                 print $ "listening topic: "
                 print topic
-                void $ async $ listenPostgres topic connListen chan
-            eventSourceAppIO (event conn chan) req sendResponse
-        event conn chan = do
-            query <- readChan chan
-            result <- Session.run (Session.statement (params query) (select $ sql query)) conn
-            either print (\rows -> forM_ rows $ \payload -> ServerEvent Nothing Nothing [string8 $ unpack payload]) result
+                void $ async $ listenPostgres topic connListen conn chan
+            eventSourceAppIO (readChan chan) req sendResponse
+        -- event conn chan = do
+        --     query <- readChan chan
+        --     result <- Session.run (Session.statement (params query) (select $ sql query)) conn
+        --     either print (\rows -> forM_ rows $ \payload -> ServerEvent Nothing Nothing [string8 $ unpack payload]) result
 
         headers :: Middleware
         headers = addHeaders [
